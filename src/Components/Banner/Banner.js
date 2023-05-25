c059a1293dc4d2ced8eda8d6ca7a149b11874ee6
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-05-25 13:45:55 -0300

    banner

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img src="/img/banner-relvis-delivery.png" alt="banner-relvis-delivery"/>
+    </header>
+  );
+}
+
+export default Banner;
