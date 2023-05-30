e9e342f50cb84859f44fbe9894c04df85969683c
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-05-29 22:00:06 -0300

    recode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Banner from "./Components/Banner/Banner";
 import Footer from "./Components/Footer/Footer";
 import Main from "./Components/Main/Main";
 
-function App() {
-  
+function App() {  
   return (
     <>
       <Banner/>
