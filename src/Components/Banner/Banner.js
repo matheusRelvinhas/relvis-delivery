f268a2b23ad583437a18ba7d214bbf3fe4254747
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-05-30 01:04:15 -0300

    img

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css';
 const Banner = () => {
   return (
     <header className="banner">
-      <img src="/img/banner-relvis-delivery.png" alt="banner-relvis-delivery"/>
+      <img src="https://imgcloud.gateway.storjshare.io/files/c7e5a8ff-2da2-4cb4-ba6a-b584eb3c4e3b/e1db49e4-44b1-4298-a031-7ef7a3c26a77.png?response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=jwgub4adpyckzzfkho2eqzrmi4iq%2F20230530%2Fus1%2Fs3%2Faws4_request&X-Amz-Date=20230530T034107Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=044dfbc7796d254acb7ce00e685d076fad05e124b1ddb75f89f4ea7c98c516cc" alt="banner-relvis-delivery"/>
     </header>
   );
 }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -6,14 +6,14 @@ const Main = () => {
     title: 'Peça pelo Whats',
     text: 'Mande uma mensagem e vamos te atender',
     url: 'https://wa.me/5531971451910',
-    imagens: ['/img/whats01-card.png','/img/whats02-card.png']
+    imagens: ['https://imgcloud.gateway.storjshare.io/files/c7e5a8ff-2da2-4cb4-ba6a-b584eb3c4e3b/f1aeb2b8-1afe-4875-8990-778f9fef1c46.png?response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=jwgub4adpyckzzfkho2eqzrmi4iq%2F20230530%2Fus1%2Fs3%2Faws4_request&X-Amz-Date=20230530T035239Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=1fb3f852d31c1e41a65a57d5473338cd347c0f8ada75f38b5f6de6295db4747c','https://imgcloud.gateway.storjshare.io/files/c7e5a8ff-2da2-4cb4-ba6a-b584eb3c4e3b/07c3d19b-f442-4a4d-872f-4b7f9a708525.png?response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=jwgub4adpyckzzfkho2eqzrmi4iq%2F20230530%2Fus1%2Fs3%2Faws4_request&X-Amz-Date=20230530T035341Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=1ec5bb5abdbc0ce0b43d60272e45d88305996bb1ea50eb56d224752e5185c1b4']
   }
 
   const ifood = {
     title: 'Peça pelo Ifood',
     text: 'Consulte nossas promoções no ifood',
     url: 'https://www.ifood.com.br/delivery/belo-horizonte-mg/relvis-delivery-padre-eustaquio/',
-    imagens: ['/img/ifood01-card.png', '/img/ifood02-card.png', '/img/ifood03-card.png']
+    imagens: ['https://imgcloud.gateway.storjshare.io/files/c7e5a8ff-2da2-4cb4-ba6a-b584eb3c4e3b/b3635396-e4d1-460f-9ba9-8c5200a526ed.png?response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=jwgub4adpyckzzfkho2eqzrmi4iq%2F20230530%2Fus1%2Fs3%2Faws4_request&X-Amz-Date=20230530T035959Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Signature=4700043bc608e83f4718b8e3a94c7e859882ab03ccb6bfb99345e39cb450f4f1', 'https://imgcloud.gateway.storjshare.io/files/c7e5a8ff-2da2-4cb4-ba6a-b584eb3c4e3b/508115d9-3668-4169-9457-fd1b5db733db.png?response-content-type=image%2Fpng&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=jwgub4adpyckzzfkho2eqzrmi4iq%2F20230530%2Fus1%2Fs3%2Faws4_request&X-Amz-Date=20230530T040048Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3599&X-Amz-Signature=3218d06eaec57a7e09bc910a36a1718ff658eaacb01b4028559480d4b094ca9d', '']
   }
 
   const bebidas = {
